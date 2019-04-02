8731904be079cec3eae4296a72af57906acfac72
liuboheng <lbh9311@sina.com>
2019-04-02 14:02:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -9,15 +9,37 @@ import speakeasy from "speakeasy"
 class App extends Component {
   @observable
   secretCode = ''
+  @observable
+  token = ''
   handlechangeSC = (e)=>{
     this.secretCode = e.target.value;
   }
+
+  saveSC = () => {
+    window.localStorage.setItem('sc',this.secretCode)
+  }
+
+  genToken = () => {
+    console.log(this.secretCode)
+    const token = speakeasy.totp({
+      secret: 'w5fmpnpvg2urnzza',
+      encoding: 'base32'
+    });
+    this.token = token
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <input type="text" value={this.secretCode} onChange={this.handlechangeSC} />
+          <div>
+            <input type="text" value={this.secretCode} onChange={this.handlechangeSC} />
+            <button onClick={this.saveSC}> 保存秘钥 </button>
+            <button onClick={this.genToken}>生成验证码</button>
+          </div>
+          <div>验证码：{this.token}</div>
         </header>
       </div>
     );
