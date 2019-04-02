82d66a21383bb34b97db63541fcf844b983dd528
liuboheng <lbh9311@sina.com>
2019-04-02 13:43:16 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {observable} from 'mobx'
+import {observer} from 'mobx-react'
+// import logo from './logo.svg';
 import './App.css';
+import speakeasy from "speakeasy"
 
+@observer
 class App extends Component {
+  @observable
+  secretCode = ''
+  handlechangeSC = (e)=>{
+    this.secretCode = e.target.value;
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <input type="text" value={this.secretCode} onChange={this.handlechangeSC} />
         </header>
       </div>
     );
